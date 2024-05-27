9769306c17731ca66474f200448d2b0890bd7be0
Nicole Bond <nicbond22@gmail.com>
2024-05-27 12:05:06 -0400

    Initial Commit

---
+++ Ready4Life/Ready4LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ready4LifeApp.swift
+//  Ready4Life
+//
+//  Created by Scholar on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Ready4LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
